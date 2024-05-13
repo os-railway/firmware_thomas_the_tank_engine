84be50fd19aa5f9206a9a82e69def2955645e7ff
Peter Siegmund <developer@mars3142.org>
2024-05-13 20:58:56 +0200

    current stage
    Signed-off-by: Peter Siegmund <developer@mars3142.org>

--- main/main.c
+++ main/main.c
@@ -1,23 +1,19 @@
-#include <stdio.h>
 #include <esp_task_wdt.h>
+#include <stdio.h>
 
 #include "osr_ble.h"
 
-void setup()
-{
-    osr_ble_init();
+void setup() {
+  osr_ble_init();
 }
 
-void loop()
-{
-    ESP_ERROR_CHECK(esp_task_wdt_reset());
+void loop() {
+  ESP_ERROR_CHECK(esp_task_wdt_reset());
 }
 
-void app_main()
-{
-    setup();
-    while (1)
-    {
-        loop();
-    }
+void app_main() {
+  setup();
+  while (1) {
+    loop();
+  }
 }
